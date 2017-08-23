1cf6b6e0738351f803492354fa53208855cc02ae
Sahana Balram <sahana2008balram@gmail.com>
2017-08-22 19:34:08 -0700

    create foldres and files respectively

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        question1 : "What is the name of Harry Potter's Owl ?",
+        options : ["Hedwig","Ginger","Coco","Grape","Vocal","Churo"],
+        answer : "Hedwig"
+    },
+
+    {
+        question2: "What is the name of Hermoine Granger's Cat ?",
+        options: ["Hedwig","Crookshanks","Coco","Grape","Vocal","Churo"],
+        answer: "Crookshanks"
+    },
+
+    {
+        question3: "What game did Ron play the best?",
+        options: ["Soccer","Qudditch","Wizard Chess","BasketBall","Tennis","Swimming"],
+        answer: "Wizard Chess"
+
+    },
+
+    {
+        question4: "What is the name of the HeadMaster of Hogwarts?",
+        options: ["Professor Snape", "Dolores Umbridge","Rubes Hagrid","Professor Quirrell","Professor DumbelDore","Lord Voldemort"],
+        answer: "Professor DumbelDore"
+    },
+
+    {
+        question5: "What are the names of Harry Potter's parents?",
+        options: ["Dudleys","Longbottoms","Sirius Black","James and Lilly Potter","Arthur Weasly","The Granger's"]
+    }
+];
